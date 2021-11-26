54a7f1e9614e08e4b824f5178f0f5c66a2f95b04
LarisaGimbu <gimbularisa@gmail.com>
2021-11-26 12:51:42 +0100

    header

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,22 @@
 <template>
   <div>
-    <div class="container">
-      ciao
-    </div>
+    <Header />
   </div>
 </template>
 
 <script>
+import Header from './components/Header.vue'
 
 export default {
   name: 'App',
   components: {
-    
+    Header,
   }
 }
 </script>
 
 <style lang="scss">
 @import './assets/style/general.scss';
+@import './assets/style/vars.scss';
 
 </style>
---
+++ src/components/Header.vue
@@ -0,0 +1,26 @@
+<template>
+  <header>
+    <div class="logo">
+      <i class="fab fa-spotify p-2"></i>
+    </div>
+  </header>
+</template>
+
+<script>
+export default {
+  name: 'Header'
+}
+</script>
+
+<style lang="scss">
+@import '../assets/style/vars.scss';
+
+header{
+  background-color: $primary-color;
+  .fa-spotify{
+    font-size: 2rem;
+    color: $logo-color;
+  }
+}
+
+</style>
