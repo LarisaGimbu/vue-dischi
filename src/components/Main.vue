a6d90c660d4200c4a9b73356a46a3fa472b01054
LarisaGimbu <gimbularisa@gmail.com>
2021-11-29 16:20:35 +0100

    genre filter

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -1,11 +1,12 @@
 <template>
   <main>
+    <Select @sendSearch='performSearch'/>
     <div class="container">
       <div 
       v-if="loading"
       class="row justify-content-center pt-5">
         <SongsBoxes 
-        v-for="(song, index) in songs"
+        v-for="(song, index) in filteredSongs"
         :key="index"
         :song="song" />
       </div>
@@ -20,18 +21,21 @@
 <script>
 import SongsBoxes from './SongsBoxes.vue';
 import Loading from './Loading.vue';
+import Select from './Select.vue';
 import axios from 'axios';
 
 export default {
   name: 'Main',
   components:{
     SongsBoxes,
-    Loading
+    Loading,
+    Select
   },
   data(){
     return{
       songs: [],
       loading: false,
+      textToSearch:''
     }
   },
   methods:{
@@ -44,6 +48,21 @@ export default {
         .catch((error) =>{
           console.log(error);
         })
+    },
+    performSearch(text){
+      this.textToSearch = text
+      console.log(this.textToSearch);
+
+    }
+  },
+  computed:{
+    filteredSongs(){
+      if(this.textToSearch === ''){
+        return this.songs;
+      }
+      return this.songs.filter(song =>{
+        return song.genre.includes(this.textToSearch);
+      })
     }
   },
   mounted(){
@@ -56,6 +75,7 @@ export default {
 @import '../assets/style/vars.scss';
 
 main{
+  position: relative;
   width: 100%;
   min-height: calc(100vh - 48px);
   background-color: $main-color;
---
+++ src/components/Select.vue
@@ -0,0 +1,38 @@
+<template>
+    <select 
+    v-model="genreToSearch"
+    @change="$emit('sendSearch',genreToSearch)"
+    class="form-select" aria-label="Default select example">
+      <option selected>Selezionare un genere</option>
+      <option value="Rock">Rock</option>
+      <option value="Pop">Pop</option>
+      <option value="Jazz">Jazz</option>
+      <option value="Metal">Metal</option>
+    </select>
+</template>
+
+<script>
+export default {
+  name: 'Select',
+  data(){
+    return{
+      genreToSearch: '',
+    }
+  },
+}
+</script>
+
+<style lang='scss'>
+@import '../assets/style/vars.scss';
+
+  select.form-select{
+    position: absolute;
+    top: -45px;
+    right: 5px;
+    width: 210px;
+    background-color: lighten($primary-color, 10%);
+    border: 2px solid $main-color;
+    color: white;
+  }
+
+</style>
